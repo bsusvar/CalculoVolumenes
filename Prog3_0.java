7844afe11193030abc6662d0c076f0eea48129db
Davidmzv <davidm.mv54@gmail.com>
2024-11-29 15:51:49 +0100

    Añadir longitudny formula

---
+++ Prog3_0.java
@@ -0,0 +1,25 @@
+package calculoVolumenes;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Prog3_0 {
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio = leerradio.nextInt();
+		double volumen;
+		volumen = (4 * (Math.PI) * (Math.pow(radio, 3)) / 3);
+		System.out.println("El volumen de la esfera cuyo radio " + radio + "es " + volumen);
+		System.out.println("Finalizado Version 1");
+		double area;
+		area = (Math.PI) * (Math.pow(radio, 2));
+		System.out.println("El área de la esfera cuyo radio " + radio + " es" + area);
+		System.out.println("Finalizado Version 2");
+
+		double longitud;
+		longitud = 2 * (Math.PI) * radio;
+	}
+}
